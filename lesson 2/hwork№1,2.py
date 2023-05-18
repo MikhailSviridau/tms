3e8e036f232f018000cf93aa5bf34f4d5a4245fe
MikhailSviridau <mikeelaginn@gmail.com>
2023-05-18 11:52:15 +0300

    lesson1

---
+++ lesson 2/hwork№1,2.py
@@ -0,0 +1,10 @@
+variable1 = (1,2,3,4,5)
+variable2 = variable1
+variable3 = variable2
+print(variable1, variable2, variable3)
+print(id(variable1), id(variable2), id(variable3))
+
+variable4 = (1,2,3,4)
+variable5 = [1,2,3,4]
+print(variable4, variable5)
+print(id(variable4), id(variable5))
