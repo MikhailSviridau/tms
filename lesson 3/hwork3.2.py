0df8753bc53bd07cc57b3a107d98d7354f9bc6be
MikhailSviridau <mikeelaginn@gmail.com>
2023-05-23 00:16:07 +0300

    lesson3

---
+++ lesson 3/hwork3.2.py
@@ -0,0 +1,13 @@
+name = input('Введите имя...')
+age = int(input('Введите возраст...'))
+if age == 0 or age < 0:
+    print('Ошибка, повторите ввод,', name)
+elif 0 < age < 10:
+    print("Привет шкет,", name)
+elif 10 <= age <= 18:
+    print('Как жизнь?', name)
+elif 18 < age < 100:
+    print('Что желаете,', name)
+else:
+    print(name,', вы лжете - в наше время столько не живут...')
+
